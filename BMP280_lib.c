eb2d250110d0fdf4b4bf1c941e2276d539f4f48f
Гаврилова Иван <discover1977@yandex.ru>
2016-12-07 14:27:06 +0300

    Create BMP280_lib.c, BMP280_lib.h

---
+++ BMP280_lib.c
@@ -0,0 +1,8 @@
+/*
+ * BMP280_lib.c
+ *
+ *  Created on: 7 дек. 2016 г.
+ *      Author: gavrilov.iv
+ */
+
+
---
+++ BMP280_lib.h
@@ -0,0 +1,13 @@
+/*
+ * BMP280_lib.h
+ *
+ *  Created on: 7 дек. 2016 г.
+ *      Author: gavrilov.iv
+ */
+
+#ifndef BMP280_LIB_BMP280_LIB_H_
+#define BMP280_LIB_BMP280_LIB_H_
+
+
+
+#endif /* BMP280_LIB_BMP280_LIB_H_ */
